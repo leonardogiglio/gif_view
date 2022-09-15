86a95be95e426eb5544f81bbb907fdb3e134ce50
leonardo.giglio <giglioleonardo@hotmail.com>
2022-09-15 12:29:08 -0300

    Mensaje Cargando

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -11,6 +11,7 @@ const GifGrid = ({category}) => {
   return (
     <>
         <h3>{category}</h3>
+        {isLoading && <h2>Cargando...</h2>}
         <div className='card-grid'>
             {
                 images.map((image) => (

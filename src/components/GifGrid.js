4a2975251968e09dc195d3d7c8e7df861f661020
leonardo.giglio <giglioleonardo@hotmail.com>
2022-09-15 11:52:03 -0300

    Gif Item

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { getGifs } from '../helpers/getGifs';
+import GifItem from './GifItem';
 
 const GifGrid = ({category}) => {
 
@@ -17,13 +18,16 @@ const GifGrid = ({category}) => {
   return (
     <>
         <h3>{category}</h3>
-        <ol>
+        <div className='card-grid'>
             {
-                images.map(({id,title}) => (
-                    <li key={id}>{title}</li>
+                images.map((image) => (
+                   <GifItem 
+                   key={image.id}
+                   {... image }
+                   />
                 ))
             }
-        </ol>
+        </div>
     </>
   )
 }
